2db9864107addf0c00f12b16705bd7d8a6dfc942
tranceaddict921 <tranceaddict921@gmail.com>
2022-08-06 02:58:11 -0700

    modified server js, ins controller and routes to get company names

--- smui/routes/insurance_company.js
+++ smui/routes/insurance_company.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 const InsuranceRating = require("../controllers/InsuranceCompany");
 
-router.get("/ins_rating_name", async (req, res) => {
+router.get("/ins_company_names", async (req, res) => {
     let ir = await InsuranceRating('Others');
     console.log(ir);
-    res.status(200).send({ "insurance_rating_name": ir });
+    res.status(200).send({ "insurance_company_name": ir });
 
 });
 module.exports = router;
--- smui/server.js
+++ smui/server.js
@@ -21,8 +21,8 @@ app.use("/a", CalculationRoutes);
 const InsuranceTermRoute = require("./routes/insurance_rating");
 app.use("/bc",InsuranceTermRoute);
 
-const InsuranceRatingRoute = require("./routes/insurance_company");
-app.use("/bd",InsuranceRatingRoute);
+const InsuranceCompanyRoute = require("./routes/insurance_company");
+app.use("/bd",InsuranceCompanyRoute);
 
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}!`);
