7af5a2362429043ee934c28f0628dd027d7b10e0
tranceaddict921 <tranceaddict921@gmail.com>
2022-08-06 06:41:08 -0700

    wrote functions for quotations

--- smui/routes/Calculations.js
+++ smui/routes/Calculations.js
@@ -537,7 +537,64 @@ router.post("/Calculate", async (req, res) => {
 		}
 	}
 
-	// LCP Calculation With Percent Step and Unhedge
+
+//function for calculation of LCP with hedge and unhedge date
+function LCP_Min_Max_Quotes(pmntStartDate,pmntEndDate,pmntMode,pmntAmount,base_rate_max,base_rate_min){
+
+	try {
+		var sum = base_rate_min;
+		var sumc = base_rate_max;
+		var m = 0.0;
+		var payments = pmntAmount;
+		var freq = 0;
+		if (pmntMode == "Weekly") {
+			var diff = (pmntEndDate.getTime() - pmntStartDate.getTime()) / 1000;
+			diff /= 60 * 60 * 24 * 7;
+			freq = Math.abs(Math.round(diff));
+			m = 52.0;
+		}
+		if (pmntMode == "Monthly") {
+			freq =
+				pmntEndDate.getMonth() -
+				pmntStartDate.getMonth() +
+				12 * (pmntEndDate.getFullYear() - pmntStartDate.getFullYear());
+			m = 12.0;
+		}
+		if (pmntMode == "Quarterly") {
+			var beginDate = Moment(pmntStartDate);
+			var endDate = Moment(pmntEndDate);
+			freq = Math.floor(endDate.diff(beginDate, "months") / 3);
+			m = 4.0;
+		}
+		if (pmntMode == "Semiannually") {
+			x = pmntEndDate.getFullYear() - pmntStartDate.getFullYear();
+			freq = x * 2;
+			m = 2.0;
+		}
+		if (pmntMode == "Annually") {
+			freq = pmntEndDate.getFullYear() - pmntStartDate.getFullYear();
+			m = 1.0;
+		}
+		var r = sum / m;
+		var rc = sumc / m;
+		var rb = 0.055 / m;
+
+
+		var pva = payments + (payments * (1 - (1 + r) ** -(freq - 1))) / r;
+		var pvc = payments + (payments * (1 - (1 + rc) ** -(freq - 1))) / rc;
+		var benbb = payments + (payments * (1 - (1 + rb) ** -(freq - 1))) / rb;
+		console.log("Min Quotation", pva);
+		console.log("Max Quotation", pvc);
+		console.log("Beneficiary Protection: ", benbb);
+
+	} catch (err) {
+		let error = {
+			status: 500,
+			error: "An error occurred. Please try again later.",
+		};
+		return res.send(error);
+	}
+
 
 	if (data.PercentStep > 0 && productType == "LCP") {
 		// Calculation of Life Span is here
@@ -803,6 +860,73 @@ router.post("/Calculate", async (req, res) => {
 				"</p>";
 			sendPayload.html_ = html_output;
 		}
+
+}
+
+function GP_Min_Max_Quotes(pmntStartDate,pmntEndDate,pmntMode,pmntAmount,ins_imp_max,ins_imp_min){
+	try {
+		var diff_in_years_gp =
+			pmntEndDate.getFullYear() - pmntStartDate.getFullYear();
+		if (diff_in_years_gp > 10 || diff_in_years_gp < 10) {
+			pmntEndDate = new Date(
+				pmntStartDate.getTime() + 3650 * 24 * 60 * 60 * 1000
+			);
+		}
+		var cr = ins_imp_min;
+		var crc = ins_imp_max;
+		var m = 0.0;
+
+		var diff_in_years_gp =
+			pmntEndDate.getFullYear() - pmntStartDate.getFullYear();
+
+		//Checking Ending Date of GP
+
+		var payments = pmntAmount;
+		var freq = 0;
+		//pmntEndDate.setMonth(pmntStartDate.getMonth() + 120);
+		if (pmntMode == "Weekly") {
+			var diff = (pmntEndDate.getTime() - pmntStartDate.getTime()) / 1000;
+			diff /= 60 * 60 * 24 * 7;
+			freq = Math.abs(Math.round(diff));
+			m = 52.0;
+		}
+		if (pmntMode == "Monthly") {
+			freq =
+				pmntEndDate.getMonth() -
+				pmntStartDate.getMonth() +
+				12 * (pmntEndDate.getFullYear() - pmntStartDate.getFullYear());
+			m = 12.0;
+		}
+		if (pmntMode == "Quarterly") {
+			var beginDate = Moment(data.PaymentStartDate);
+			var endDate = Moment(pmntEndDate);
+			freq = Math.floor(endDate.diff(beginDate, "months") / 3);
+			m = 4.0;
+		}
+		if (pmntMode == "Semiannually") {
+			x = pmntEndDate.getFullYear() - pmntStartDate.getFullYear();
+			freq = x * 2;
+			m = 2.0;
+		}
+		if (pmntMode == "Annually") {
+			freq = pmntEndDate.getFullYear() - pmntStartDate.getFullYear();
+			m = 1.0;
+		}
+		var r = cr / m;
+		var rc = crc / m;
+
+		var pva = payments + (payments * (1 - (1 + r) ** -(freq - 1))) / r;
+		var pvc = payments + (payments * (1 - (1 + rc) ** -(freq - 1))) / rc;
+		
+		console.log("Max Quote GP",pva);
+		console.log("Min Quote GP",pvc);
+
+		
+	} catch (err) {
+		console.log("err in getting results: ", err);
+	}
+
+
 	}
 });
 
