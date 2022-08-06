72a645af5711fa5eead9a88f1c3206aea2bee524
tranceaddict921 <tranceaddict921@gmail.com>
2022-08-06 10:37:33 -0700

    modified insurance rating

---
+++ smui/controllers/InsuranceCompany.js
@@ -0,0 +1,12 @@
+const JsonConverter = require("../helperFuntions/JsonConverter");
+const SqlQueryHandler = require("../helperFuntions/SqlQueryHandler");
+
+
+module.exports = async function InsuranceRating() {
+	query = `select * from insurance_rating;`;
+    console.log(query);
+	let company_names = await JsonConverter(await SqlQueryHandler(query)); 
+	console.log(company_names);
+	return company_names;
+};
+
