fd4f10da2838547d06948f14149505a0a285b221
tranceaddict921 <tranceaddict921@gmail.com>
2022-08-06 02:47:59 -0700

    changed port to 5000

--- smui/server.js
+++ smui/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 var config = require("../smui/db_connection/connection.js");
-const port = 3000;
+const port = 5000;
 app.use(express.json());
 app.use(cors());
 
