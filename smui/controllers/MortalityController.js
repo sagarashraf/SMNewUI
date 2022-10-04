d06d785405361d37c748b11517234ab6999bc109
sagarashraf93 <sagarashraf93@gmail.com>
2022-10-04 23:02:41 +0500

    Constant

--- smui/controllers/MortalityController.js
+++ smui/controllers/MortalityController.js
@@ -1,3 +1,6 @@
+const WOMAN_GENDER_FACTOR = 0.25;
+const MALE_GENDER_FACTOR = 0.3;
+const CONSTANT_SETUP = 1;
 module.exports = async function MortalityController(
 	Medical,
 	lifestyle,
@@ -10,9 +13,9 @@ module.exports = async function MortalityController(
 	var ImpactValue = await [].concat.apply([], valueList);
 	console.log("Values list ", ImpactValue);
 	if (sex === 0) {
-		ImpactValue.push(0.3, 1);
+		ImpactValue.push(MALE_GENDER_FACTOR, CONSTANT_SETUP);
 	} else {
-		ImpactValue.push(0.25, 1);
+		ImpactValue.push(WOMAN_GENDER_FACTOR, CONSTANT_SETUP);
 	}
 	let yourMortalityRate = ImpactValue.reduce((a, b) => a + b, 0);
 	return yourMortalityRate;
