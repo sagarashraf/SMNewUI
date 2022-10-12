204661cbe974b9e687d571c56b6050cf3bbd72cf
sagarashraf93 <sagarashraf93@gmail.com>
2022-10-12 20:35:58 +0500

    Date diff updated

--- smui/controllers/LCPQuotesWithoutPercentStep.js
+++ smui/controllers/LCPQuotesWithoutPercentStep.js
@@ -47,32 +47,30 @@ module.exports = async function LCP_Min_Max_Quotes(
 		var rc = sumc / m;
 		var rb = 0.055 / m;
 
-
 		var pva = payments + (payments * (1 - (1 + r) ** -(freq - 1))) / r;
 		var pvc = payments + (payments * (1 - (1 + rc) ** -(freq - 1))) / rc;
 		var benbb = payments + (payments * (1 - (1 + rb) ** -(freq - 1))) / rb;
-
-
 
 		var ann_interest_rate = (1 + sum / m) ** m - 1;
 		var ann_interest_rate_c = (1 + sumc / m) ** m - 1;
 		var ann_interest_rate_b = (1 + 0.054 / m) ** m - 1;
 
-		var pvad = DateDiff(pmntEndDate, pmntStartDate);
-		pva = pva/(1+ann_interest_rate)**(pvad/365)
-		pvc = pvc/(1+ann_interest_rate_c)**(pvad/365)
-		benbb = benbb/(1+ann_interest_rate_b)**(pvad/365)
-
+		var pvad = await DateDiff(pmntEndDate, pmntStartDate);
+		pva = pva / (1 + ann_interest_rate) ** (pvad / 365);
+		pvc = pvc / (1 + ann_interest_rate_c) ** (pvad / 365);
+		benbb = benbb / (1 + ann_interest_rate_b) ** (pvad / 365);
 
 		console.log("Min Quotation", pva);
 		console.log("Max Quotation", pvc);
-		console.log("Beneficiary Protection: ", benbb);
+		console.log("pvad pvad: ", pvad);
+		console.log("pmntEndDate, pmntStartDate =>>>", pmntEndDate, pmntStartDate);
 		return {
 			LCPMinQuotesWithoutAI: pvc,
 			LCPMaxQuotesWithoutAI: pva,
 			LCPBEN: benbb,
 		};
 	} catch (err) {
+		console.log("Zeeshan error", err);
 		return {
 			error: "Calculation Error",
 		};
